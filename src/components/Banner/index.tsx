949cd452db1b14ce7d31822c79c008e9dff01ec4
Raul26_tech <rauletec2020@gmail.com>
2023-03-28 18:57:00 -0300

    Finalizando Banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,16 +1,19 @@
-import Button from '../Buttom';
+import { ButtonLink } from '../Buttom';
 
 export default function Banner() {
     return (
         <>
-            <div className="w-full h-[20rem] bg-theme-blue-50 rounded-md shadow-md p-6 text-white font-roboto">
+            <div className="w-full md:h-[20rem] bg-theme-blue-50 rounded-md shadow-md p-6 text-white font-roboto space-y-[5rem]">
                 <div className="w-full flex justify-center items-center text-center">
                     <span className="text-white font-bold text-3xl">
                         Seja muito bem-vindo(a) ao nosso E-commerce
                     </span>
                 </div>
-                <div className="w-full h-full flex justify-center items-center">
-                    <Button>Saiba mais</Button>
+                <div className="w-full flex flex-col justify-center items-center p-3 space-y-4">
+                    <span className="text-white font-semibold text-center text-xl">
+                        Fique por dentro de nossas ofertas e produtos
+                    </span>
+                    <ButtonLink url="/products">Saiba mais</ButtonLink>
                 </div>
             </div>
         </>
--- src/components/Buttom/index.tsx
+++ src/components/Buttom/index.tsx
@@ -3,7 +3,7 @@ import { Link, LinkProps } from 'react-router-dom';
 
 const COLORS = {
     default:
-        'bg-theme-blue border-2 border-solid-theme-green-50 hover:bg-theme-blue-50-hover',
+        'bg-theme-blue border-2 border-theme-green-50 hover:bg-theme-blue-50-hover',
     primary: '',
     secondary: '',
     danger: '',
@@ -27,17 +27,19 @@ export default function Button({
     children,
     pattern = 'default',
     addClassName = '',
+    ...rest
 }: IButtomProps) {
     return (
-        <div
+        <button
             className={`w-auto h-12 flex justify-center items-center p-3 rounded-md cursor-pointer ${addClassName} ${COLORS[pattern]}`}
+            {...rest}
         >
             {children}
-        </div>
+        </button>
     );
 }
 
-interface IButtonLink extends LinkProps {
+interface IButtonLink {
     children: ReactNode;
     className?: string;
     pattern?:
@@ -48,6 +50,7 @@ interface IButtonLink extends LinkProps {
         | 'success'
         | 'warning';
     addClassName?: string;
+    url?: string;
 }
 
 export function ButtonLink({
@@ -55,11 +58,11 @@ export function ButtonLink({
     pattern = 'default',
     className = `w-auto h-12 flex justify-center items-center p-3 rounded-md cursor-pointer ${COLORS[pattern]}`,
     addClassName,
-    ...rest
+    url = '',
 }: IButtonLink) {
     return (
-        <Link className={` ${addClassName} `} {...rest}>
-            {children}
-        </Link>
+        <button className={` ${className} ${addClassName} `}>
+            <a href={url}>{children}</a>
+        </button>
     );
 }
