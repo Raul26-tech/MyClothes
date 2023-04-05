2d352098f226c148e540a89a839189423716e764
Raul26_tech <rauletec2020@gmail.com>
2023-04-04 21:02:30 -0300

    Refatorando código do componente Slide

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,64 +1,40 @@
 import { ButtonHTMLAttributes, ReactNode, useState } from 'react';
 import { HiArrowLeft, HiArrowRight } from 'react-icons/hi';
-import image1 from '../../assets/placa.jpg';
-import image2 from '../../assets/processador.jpg';
-import image3 from '../../assets/rgb_hyperx.jpg';
-
-const slide = [
-    { url: `${image1}` },
-    { url: `${image2}` },
-    { url: `${image3}` },
-];
+import Button from '../Buttom';
 
 interface ISliderProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
-    forNext?: () => void;
-    toGoBack?: () => void;
+    prevSlide?: () => void;
+    nextSlide?: () => void;
     addClassName?: string;
 }
 
 export default function Slider({
     children,
-    forNext,
-    toGoBack,
     addClassName = '',
+    prevSlide,
+    nextSlide,
     ...rest
 }: ISliderProps) {
-    const [current, setCurrent] = useState(0);
-
-    const prevSlide = () => {
-        const currentIndex = current === 0;
-        const currentValue = (currentIndex as unknown as number)
-            ? slide.length - 1
-            : (currentIndex as unknown as number) + 1;
-        setCurrent(currentValue);
-    };
-
-    const nextSlide = () => {
-        const currentIndex = current === slide.length - 1;
-        const currentValue = currentIndex
-            ? 0
-            : (current as unknown as number) + 1;
-        setCurrent(currentValue);
-    };
-
     return (
         <div
             className={`w-full md:max-w-[1400px] lg:m-w-[1600px] h-auto m-auto p-6 relative rounded-md shadow-xl bg-white group ${addClassName}`}
         >
-            <div
-                style={{ backgroundImage: `url(${slide[current].url})` }}
-                className="w-full h-[30rem] p-6 rounded-2xl bg-center bg-contain bg-no-repeat shadow-lg duration-500"
+            {children}
+            <Button
+                pattern="primary"
+                onClick={prevSlide}
+                className="hidden group-hover:block absolute p-3 top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full bg-theme-blue-50 text-white cursor-pointer"
             >
-                {children}
-            </div>
-
-            <div className="hidden group-hover:block absolute p-3 top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full bg-theme-blue-50 text-white cursor-pointer">
-                <HiArrowLeft size={30} onClick={prevSlide} />
-            </div>
-            <div className="hidden group-hover:block absolute p-3 top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full bg-theme-blue-50 text-white cursor-pointer">
-                <HiArrowRight size={30} onClick={nextSlide} />
-            </div>
+                <HiArrowLeft size={30} />
+            </Button>
+            <Button
+                pattern="primary"
+                onClick={nextSlide}
+                className="hidden group-hover:block absolute p-3 top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full bg-theme-blue-50 text-white cursor-pointer"
+            >
+                <HiArrowRight size={30} />
+            </Button>
         </div>
     );
 }
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,20 +1,52 @@
+import { useCallback, useRef, useState } from 'react';
 import Content from '../../components/Content';
 import Layout from '../../components/Layout';
 import Banner from '../../components/Banner';
 import Slider from '../../components/Slider';
-import { useCallback, useRef } from 'react';
+import image1 from '../../assets/placa.jpg';
+import image2 from '../../assets/processador.jpg';
+import image3 from '../../assets/rgb_hyperx.jpg';
 
 export default function Main() {
-    const handleScroolview = useCallback(() => {
-        console.log('');
-    }, []);
+    const [current, setCurrent] = useState(0);
+
+    const slide = [
+        { url: `${image1}` },
+        { url: `${image2}` },
+        { url: `${image3}` },
+    ];
+
+    const prevSlide = () => {
+        const currentIndex = current === 0;
+        const currentValue = (currentIndex as unknown as number)
+            ? slide.length - 1
+            : (currentIndex as unknown as number) + 1;
+        setCurrent(currentValue);
+    };
+
+    const nextSlide = () => {
+        const currentIndex = current === slide.length - 1;
+        const currentValue = currentIndex
+            ? 0
+            : (current as unknown as number) + 1;
+        setCurrent(currentValue);
+    };
 
     return (
         <Layout>
             <Content>
                 <Banner />
-                <Slider addClassName="mt-6" forNext={handleScroolview}>
-                    teste
+                <Slider
+                    addClassName="mt-6"
+                    prevSlide={prevSlide}
+                    nextSlide={nextSlide}
+                >
+                    <img
+                        style={{
+                            backgroundImage: `url(${slide[current].url})`,
+                        }}
+                        className="w-full h-[30rem] p-6 rounded-2xl bg-center bg-contain bg-no-repeat shadow-lg duration-500"
+                    />
                 </Slider>
             </Content>
         </Layout>
