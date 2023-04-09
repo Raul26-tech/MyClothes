3a012f57f184264492fdc9e1930406604ff50444
Raul26_tech <rauletec2020@gmail.com>
2023-04-09 15:32:16 -0300

    Refatorando código

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Banner from '../../components/Banner';
 import Slider from '../../components/Slider';
-
 import placaComputador from '../../assets/placa.png';
 import processador from '../../assets/processador.png';
 import memoria from '../../assets/rgb_hyperx.png';
