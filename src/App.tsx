5f4b81ac0f49bdc553f3baca7160e724d5e79919
Raul26_tech <rauletec2020@gmail.com>
2023-04-11 23:37:45 -0300

    Removendo página de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { MainContext, MainProvider } from './contexts/MainContext';
+import { MainProvider } from './contexts/MainContext';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
-import Login from './pages/Login';
-import { useContext } from 'react';
 
 function App() {
-    const { user } = useContext(MainContext);
-
-    // return (
-    //     <>
-    //         <Login />
-    //     </>
-    // );
-
     return (
         <BrowserRouter>
             <MainProvider>
