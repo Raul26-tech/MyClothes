b18a9af2a46c0bdb531e64bd5e9b197ed5e00d7e
Raul26_tech <rauletec2020@gmail.com>
2023-04-12 18:05:19 -0300

    Ajustando layout dos botões

--- src/components/Buttom/index.tsx
+++ src/components/Buttom/index.tsx
@@ -2,16 +2,12 @@ import React, { ButtonHTMLAttributes, ReactNode } from 'react';
 
 const COLORS = {
     default:
-        'bg-theme-blue transition duration-400 border-2 border-theme-green-50 hover:bg-theme-blue-50-hover',
-    primary:
-        'bg-primary transition duration-400 border-2 border-white hover:bg-theme-blue-50',
-    secondary:
-        'bg-secondary transition duration-400 border-2 hover:border-orange-500',
-    danger: 'bg-danger transition duration-400 border-2 hover:border-red-600',
-    success:
-        'bg-success transition duration-400 border-2 hover:border-green-600',
-    warning:
-        'bg-warning transition duration-400 border-2 hover:border-yellow-400',
+        'bg-theme-blue transition duration-400  hover:bg-theme-blue-50-hover',
+    primary: 'bg-primary transition duration-400 hover:bg-theme-blue-50',
+    secondary: 'bg-secondary transition duration-400 ',
+    danger: 'bg-danger transition duration-400',
+    success: 'bg-success transition duration-400',
+    warning: 'bg-warning transition duration-400',
 };
 
 interface IButtomProps extends ButtonHTMLAttributes<HTMLButtonElement> {
