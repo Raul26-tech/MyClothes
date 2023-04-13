311ab34f5071fe9178a7b2599944fdefd9b8bfb5
Raul26_tech <rauletec2020@gmail.com>
2023-04-12 21:03:16 -0300

    Criando componente Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,66 @@
+import {
+    ForwardRefRenderFunction,
+    InputHTMLAttributes,
+    forwardRef,
+} from 'react';
+
+import { FieldError } from 'react-hook-form';
+
+interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+    addClassName?: string;
+    labelClassName?: string;
+    className?: string;
+    disabledInput?: string;
+    error?: FieldError;
+    label?: string;
+}
+
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, IInputProps> = (
+    {
+        error = null,
+        addClassName = '',
+        labelClassName = 'block text-sm text-slate-600 mb-1 font-normal font-roboto',
+        className = `
+        min-w-full
+        p-3
+        outline-none
+        text-slate-600
+        bg-white
+        border-[1px]
+        border-slate-400
+        shadow-xl
+        rounded-md
+        focus:border-theme-blue-50
+        focus:border-[1px]
+    `,
+
+        type = 'text',
+        label,
+        disabled,
+        disabledInput = disabled ? 'bg-slate-200' : '',
+        onBlur,
+        onFocus,
+        ...rest
+    },
+    ref
+) => {
+    return (
+        <label className={`w-full max-h-fit ${addClassName}`}>
+            {label && <span className={`${labelClassName}`}>{label}</span>}
+            <input
+                className={`${className} ${disabledInput}`}
+                type={type}
+                disabled={disabled}
+                onBlur={onBlur}
+                onFocus={onFocus}
+                ref={ref}
+                {...rest}
+            />
+            {error?.message ? (
+                <span className="text-xs text-red-400">{error.message}</span>
+            ) : null}
+        </label>
+    );
+};
+
+export const Input = forwardRef(InputBase);
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import Titles from '../../components/Titles';
 import { MainContext } from '../../contexts/MainContext';
+import { Input } from '../../components/Input';
 
 export default function Login() {
     const { signIn } = useContext(MainContext);
@@ -9,10 +10,17 @@ export default function Login() {
         <>
             <div className="w-screen h-screen p-20">
                 <div className="w-full h-full flex rounded-xl shadow-2xl">
-                    <div className="w-full h-full md:bg-[url('../assets/e-commerce-technology.gif')] bg-no-repeat bg-cover bg-center rounded-l-md opacity-95"></div>
+                    <div className="w-full h-full md:bg-[url('../assets/e-commerce-technology.gif')] bg-no-repeat bg-cover bg-center rounded-l-xl opacity-95"></div>
                     <div className="w-3/4 flex flex-col p-6 ">
                         <Titles>MyStore</Titles>
-                        teste
+                        <div className="w-full h-full flex flex-col justify-center items-center">
+                            <span className="font-montserra font-semibold text-slate-600">
+                                Acesse sua conta
+                            </span>
+                            <div className="w-full h-full p-3 justify-start items-center">
+                                <Input label="E-mail" disabled />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
