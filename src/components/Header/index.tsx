b212da8eff9e5f2408ef0f78ab77e0335911e29d
Raul26_tech <rauletec2020@gmail.com>
2023-04-13 21:12:25 -0300

    Ajustando icones do Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import { BsCart3 } from 'react-icons/bs';
 import { HiMenu } from 'react-icons/hi';
 import { BiUser } from 'react-icons/bi';
+import { useAuth } from '../../hooks/useAuth';
 
 interface IHeaderProps {
     openMenu: () => void;
 }
 
 export default function Header({ openMenu }: IHeaderProps) {
+    const { user } = useAuth();
+
     return (
         <>
             <header className="w-full h-16 md:h-24 p-3 flex justify-end firs items-center shadow-xl">
@@ -15,13 +18,16 @@ export default function Header({ openMenu }: IHeaderProps) {
                 </button>
                 <div className="w-full flex justify-end items-center">
                     <div className="h-auto flex justify-end items-center md:p-2 space-x-5">
-                        <div className="flex w-[2rem] md:w-12 h-[2rem] md:h-12">
-                            <div className="w-full flex justify-center items-center bg-theme-blue-50 p-2 rounded-full">
+                        <div className="flex w-[5rem] md:w-auto h-[2rem] md:h-12 space-x-3">
+                            <div className="flex justify-center items-center">
+                                <span>{user?.name}</span>
+                            </div>
+                            <div className="w-[3rem] flex justify-center items-center bg-theme-blue-50 p-2 rounded-full">
                                 <BiUser className="w-full h-9 md:h-12 text-white" />
                             </div>
                         </div>
-                        <div className="w-12 flex justify-center items-center p-3">
-                            <BsCart3 size={20} className="w-full h-full" />
+                        <div className="w-auto flex justify-center items-center p-3">
+                            <BsCart3 size={25} className="w-full h-full" />
                         </div>
                     </div>
                 </div>
