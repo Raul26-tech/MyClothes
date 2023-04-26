44c9650be842676ff62386518955727c1295e276
Raul26_tech <rauletec2020@gmail.com>
2023-04-26 20:08:25 -0300

    Ajustando responsividade da Página de Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -38,52 +38,52 @@ export default function Login() {
     };
 
     return (
-        <div className="w-screen h-screen p-20">
-            <div className="w-full h-full flex rounded-xl shadow-2xl">
+        <div className="w-screen h-screen md:p-20">
+            <div className="w-full h-full flex justify-center rounded-xl shadow-2xl">
                 <div className="hidden md:block w-3/4 h-full md:bg-[url('../assets/e-commerce-technology.gif')] bg-no-repeat bg-cover bg-center rounded-l-xl opacity-95" />
-                <div className="w-3/4 flex flex-col justify-center items-center p-6 ">
+                <div className="w-full h-full flex flex-col justify-center items-center p-6">
                     <Titles>MyStore</Titles>
-                    <div className="w-full h-full flex flex-col justify-center items-center">
-                        <span className="font-montserra font-semibold text-slate-600">
-                            Acesse sua conta
-                        </span>
-                        <form
-                            className="w-full h-full flex flex-col p-3 space-y-2 justify-center items-center"
-                            onSubmit={handleSubmit(handleSignIn)}
+                    <span className="font-montserra font-semibold text-slate-600">
+                        Acesse sua conta
+                    </span>
+                    <form
+                        className="w-full space-y-3 flex flex-col justify-center items-center"
+                        onSubmit={handleSubmit(handleSignIn)}
+                    >
+                        <Input
+                            label="E-mail"
+                            {...register('email', {
+                                required: true,
+                            })}
+                            addClassName="xl:w-3/4"
+                            error={formState.errors.email}
+                        />
+                        <Input
+                            label="Senha"
+                            {...register('password', {
+                                required: true,
+                            })}
+                            type="password"
+                            autoComplete="off"
+                            addClassName="xl:w-3/4"
+                            error={formState.errors.password}
+                        />
+                        <Button
+                            pattern="primary"
+                            type="submit"
+                            addClassName="w-full xl:w-3/4 text-white"
                         >
-                            <Input
-                                label="E-mail"
-                                {...register('email', {
-                                    required: true,
-                                })}
-                                error={formState.errors.email}
-                            />
-                            <Input
-                                label="Senha"
-                                {...register('password', {
-                                    required: true,
-                                })}
-                                type="password"
-                                autoComplete="off"
-                                error={formState.errors.password}
-                            />
-                            <Button
-                                pattern="primary"
-                                type="submit"
-                                addClassName="text-white md:w-full mb:mt-3"
-                            >
-                                Entrar
-                            </Button>
-                            <div className="w-full p-3 flex justify-center md:justify-end items-center">
-                                <Link to="/register" className="text-xs">
-                                    Você ainda não possui conta ? {''}
-                                    <strong className="text-theme-blue-50 underline">
-                                        Então criei uma!
-                                    </strong>
-                                </Link>
-                            </div>
-                        </form>
-                    </div>
+                            Entrar
+                        </Button>
+                        <div className="w-full md:w-3/4 flex justify-center md:justify-end items-center">
+                            <Link to="/register" className="text-xs">
+                                Você ainda não possui conta ? {''}
+                                <strong className="text-theme-blue-50 underline">
+                                    Então criei uma!
+                                </strong>
+                            </Link>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
