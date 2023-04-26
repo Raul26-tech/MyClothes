a3ab505d8263994651191964e3681b8a4ac6f160
Raul26_tech <rauletec2020@gmail.com>
2023-04-25 21:14:40 -0300

    Criando tela de Registro

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -2,8 +2,13 @@ import React, { ReactNode } from 'react';
 
 interface IContainerProps {
     children: ReactNode;
+    addClassName?: string;
 }
 
-export default function Container({ children }: IContainerProps) {
-    return <div className="flex-1 overflow-y-auto p-3">{children}</div>;
+export default function Container({ addClassName, children }: IContainerProps) {
+    return (
+        <div className={`flex-1 overflow-y-auto p-3 ${addClassName}`}>
+            {children}
+        </div>
+    );
 }
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,12 +1,46 @@
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import Titles from '../../components/Titles';
 import { Input } from '../../components/Input';
-import { useCallback, useEffect, useState } from 'react';
-import Button from '../../components/Buttom';
-import { HiArrowNarrowLeft, HiArrowNarrowRight } from 'react-icons/hi';
-import { useNavigate } from 'react-router-dom';
+import { useCallback } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { api } from '../../services/api';
 import Container from '../../components/Container';
+import Button from '../../components/Buttom';
+
+// const validations = yup.object({
+//     name: yup.string().required('Nome é obrigatório'),
+//     email: yup.string().required('E-mail obrigatório'),
+//     password: yup.string().required('Senha obrigatória'),
+//     passwordConfirm: yup
+//         .string()
+//         .required('Confirmação de senha obrigatória')
+//         .oneOf(
+//             [yup.ref('password')],
+//             'Senha e confirmação de senha não conferem'
+//         ),
+//     emailConfirm: yup
+//         .string()
+//         .required('Confirmação de e-mail é obrigatório')
+//         .oneOf(
+//             [yup.ref('email')],
+//             'E-mail e confirmação de e-mail não conferem'
+//         ),
+//     address: yup
+//         .object({
+//             postalCode: yup
+//                 .string()
+//                 .required('CEP obrigatório')
+//                 .min(8, 'CEP incompleto'),
+//             street: yup.string().required('Rua obrigatório'),
+//             number: yup.string().required('SN = Sem número'),
+//             city: yup.string().required('Cidade obrigatório'),
+//             state: yup.string().required('Estado obrigatório'),
+//             uf: yup.string().required('UF obrigatório'),
+//         })
+//         .required(),
+// });
 
 interface IViaCep {
     cep: string;
@@ -35,40 +69,33 @@ interface IRegisterProps {
 
 export default function Register() {
     const navigate = useNavigate();
-    const [accountAddress, setAccountAddress] = useState(false);
-    const [dataAccount, setDataAccount] = useState(true);
 
-    const {
-        register,
-        handleSubmit,
-        formState,
-        reset,
-        getValues,
-        setFocus,
-        watch,
-    } = useForm<IRegisterProps>({
-        defaultValues: {
-            name: '',
-            email: '',
-            password: '',
-            postalCode: '',
-            address: {
-                street: '',
-                number: '',
-                district: '',
-                city: '',
-                uf: '',
+    const { register, handleSubmit, formState, reset, getValues, setFocus } =
+        useForm<IRegisterProps>({
+            // resolver: yupResolver(validations),
+            defaultValues: {
+                name: '',
+                email: '',
+                password: '',
+                postalCode: '',
+                address: {
+                    street: '',
+                    number: '',
+                    district: '',
+                    city: '',
+                    uf: '',
+                },
             },
-        },
-    });
+        });
 
     const handleSaveRegister: SubmitHandler<IRegisterProps> = async (
         submitData
     ) => {
         try {
-            await api.post('/users', {
+            const response = await api.post('/users', {
                 ...submitData,
             });
+
             navigate('/login');
         } catch (e) {
             console.log(e);
@@ -98,15 +125,105 @@ export default function Register() {
     }, []);
 
     return (
-        <Container>
-            <div className="w-full h-full p-6">
+        <div className="w-screen h-screen">
+            <Container addClassName="w-full h-full flex flex-col justify-center items-center md:p-[5rem]">
                 <Titles>My store</Titles>
-            </div>
-            <div className="w-full h-full p-6 flex justify-center items-center rounded-md shadow-2xl drop-shadow-md">
-                <form className="grid md:grid-cols-3 p-3 bg-red-300 gap-3 space-y-3">
-                    <Input {...register('name')} />
+                <form
+                    onSubmit={handleSubmit(handleSaveRegister)}
+                    className="grid md:grid-cols-3 lg:grid-cols-4 p-3 gap-1 space-y-3"
+                >
+                    <Input
+                        label="Nome completo"
+                        {...register('name')}
+                        error={formState.errors.name}
+                        addClassName="md:col-span-4"
+                    />
+                    <Input
+                        label="E-mail"
+                        {...register('email')}
+                        error={formState.errors.email}
+                        addClassName="md:col-span-2"
+                    />
+                    <Input
+                        label="Confirmação de E-mail"
+                        {...register('emailConfirm')}
+                        error={formState.errors.emailConfirm}
+                        addClassName="md:col-span-2"
+                    />
+                    <Input
+                        label="Senha"
+                        {...register('password')}
+                        error={formState.errors.password}
+                        type="password"
+                        addClassName="md:col-span-2"
+                    />
+                    <Input
+                        label="Confirmação de senha"
+                        {...register('passwordConfirm')}
+                        error={formState.errors.passwordConfirm}
+                        type="password"
+                        addClassName="md:col-span-2"
+                    />
+                    <Input
+                        label="CEP"
+                        placeholder="Digite seu Cep"
+                        {...register('postalCode', {
+                            onBlur: handleGetPostalCode,
+                        })}
+                        error={formState.errors.postalCode}
+                        addClassName="md:col-span-1"
+                    />
+                    <Input
+                        label="Rua"
+                        {...register('address.street')}
+                        error={formState.errors.address?.street}
+                        addClassName="md:col-span-3"
+                        disabled
+                    />
+                    <Input
+                        label="Número"
+                        {...register('address.number')}
+                        error={formState.errors.address?.number}
+                        addClassName="md:col-span-1"
+                    />
+                    <Input
+                        label="Bairro"
+                        {...register('address.district')}
+                        error={formState.errors.address?.district}
+                        addClassName="md:col-span-1"
+                        disabled
+                    />
+                    <Input
+                        label="Cidade"
+                        {...register('address.city')}
+                        error={formState.errors.address?.city}
+                        addClassName="md:col-span-1"
+                        disabled
+                    />
+                    <Input
+                        label="UF"
+                        {...register('address.uf')}
+                        error={formState.errors.address?.uf}
+                        addClassName="md:col-span-1"
+                        disabled
+                    />
+                    <div className="md:col-span-4 flex justify-between items-center md:grid-cols-3 lg:grid-cols-4 p-3 gap-1 space-y-3">
+                        <Link
+                            to="/login"
+                            className="text-sm text-theme-blue-50 underline font-semibold"
+                        >
+                            Voltar para página de login
+                        </Link>
+                        <Button
+                            addClassName="text-white"
+                            pattern="primary"
+                            type="submit"
+                        >
+                            Cadastrar
+                        </Button>
+                    </div>
                 </form>
-            </div>
-        </Container>
+            </Container>
+        </div>
     );
 }
