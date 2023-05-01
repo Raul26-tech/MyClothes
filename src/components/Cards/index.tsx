c4c21c059a270e4bd15bbf7d88b2f8ab5cfd59a5
Raul26_tech <rauletec2020@gmail.com>
2023-05-01 16:21:47 -0300

    Ajustando rotas da aplicação

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -15,7 +15,7 @@ export default function Card({
     avaliation,
 }: IProductProps) {
     return (
-        <div className="w-full min-w-[15rem] p-6 flex flex-col gap-x-2 space-x-5 transition duration-300 hover:translate-y-2 bg-white rounded-sm shadow-2xl drop-shadow-md">
+        <div className="w-full min-w-[15rem] md:w-[10rem] p-6 flex flex-col gap-x-2 space-x-5 transition duration-300 hover:translate-y-2 bg-white rounded-sm shadow-2xl drop-shadow-md">
             <div className="w-full h-3/4 bg-red-300 border-[1px] border-b-slate-500">
                 <img src={picture} alt="Imagem do produto" />
             </div>
