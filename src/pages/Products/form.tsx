d53983bf82c1809f7fb60c9033ec75f2cc811c74
Raul26_tech <rauletec2020@gmail.com>
2023-05-08 19:16:33 -0300

    Iniciando a criação do form dos produtos

--- src/pages/Products/form.tsx
+++ src/pages/Products/form.tsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+import { useParams } from 'react-router-dom';
+import Layout from '../../components/Layout';
+import Content from '../../components/Content';
+import { IProductProps } from '../../components/Cards';
+
 export default function FormProduts() {
-    return <div>ProdutsForm</div>;
+    const { id } = useParams();
+    const [productRequest, setProductRequest] = useState<IProductProps>();
+
+    useEffect(() => {
+        api.get<IProductProps>(`/products/${id}`)
+            .then((res) => {
+                console.log(res.data);
+                setProductRequest(res.data);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }, []);
+
+    return (
+        <Layout>
+            <Content>
+                <div className="">{productRequest?.name}</div>
+            </Content>
+        </Layout>
+    );
 }
